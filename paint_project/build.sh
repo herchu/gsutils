fa6b7ce986bab434d8203f51efdb326fa3e2f4ac
Hernan Badenes <herchu@inorbit.ai>
2021-10-23 11:49:15 -0300

    initial Paint project and builder script

---
+++ paint_project/build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# Arma varios tableros de ejemplo con img2board, y empaqueta el proyecto
+# (cuyo template esta en 'contents') en un proyecto Gobstones: Paint.gbp
+#
+
+PRJ=Paint.gbp
+DIR=contents
+IMG_TOOL="python ../img2board/img2board.py"
+SKIN_FILE=rgb64_v1.zip
+
+B=1
+while read IMG; do
+  echo "Creando tablero Board$B.gbb de imagen $IMG..."
+  BOARD=$DIR/assets/boards/Board$B.gbb
+  $IMG_TOOL $IMG $BOARD
+  ((i=i+1))
+done <boards.txt
+
+echo "Copiando vestimenta"
+SKIN_DIR=$DIR/attires/RGB
+mkdir -p $SKIN_DIR
+pushd $SKIN_DIR
+unzip ../../../$SKIN_FILE
+popd
+
+echo "Empaquetando proyecto $PRJ"
+[ -f $PRJ ] && rm $PRJ
+pushd $DIR
+zip ../$PRJ * -r -x '*.git'
+popd
